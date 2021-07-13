971d44486e1748ed72722f1a508903a3c272777c
vivekkala2020 <vivekkala2020@gmail.com>
2021-07-13 08:04:09 +0530

    More Splits to componets

---
+++ src/app/datagridview/data-grid-view.component.ts
@@ -0,0 +1,12 @@
+import {
+  Component
+} from '@angular/core';
+
+@Component({
+  selector: 'app-data-grid-view',
+  templateUrl: './data-grid-view.component.html',
+  styleUrls: ['./data-grid-view.component.scss']
+})
+export class DataGridViewComponent {
+
+}
